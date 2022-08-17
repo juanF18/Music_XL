897d1b53b9372d9bee7ed92eb6a18f00a124430a
mrtopo1840 <juanfelipecortescastrillon@gmail.com>
2022-08-16 20:37:44 -0500

    Se agrega el sistema de rutas para usuarios logeados, se usa react-router-dom v6

--- src/routes/LoggedNavigation.js
+++ src/routes/LoggedNavigation.js
@@ -1,14 +1,25 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Album, Albums, Artist, Artists, Auth, Home, Profile } from "../pages";
 
 /**
- * Componente de navegacion entre pantallas o componentes.
- * @returns
+ * Componente de navegacion entre pantallas o componentes
+ * usando react-router-dom v6
+ * @returns los componentes segun el path en el que estemos
  */
 export function LoggedNavigation() {
+  //el parametro :id va a llegar con la clave id y el valor va a ser dinamco
+
   return (
-    <div>
-      <Button secondary>Logged Screen</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/artists" element={<Artists />} />
+        <Route path="/artists/:id" element={<Artist />} />
+        <Route path="/albums" element={<Albums />} />
+        <Route path="/albums:id" element={<Album />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
