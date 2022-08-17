fdc93baf190ad16249b33bab18727b64a7718c65
mrtopo1840 <juanfelipecortescastrillon@gmail.com>
2022-08-16 21:34:42 -0500

    Se agrega el sistema de rutas para usuarios no logeados sin uso de react-router-dom por simplicidad

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,11 +1,22 @@
-import React from "react";
-import { Button } from "semantic-ui-react";
+import React, { useState } from "react";
+import { AuthOptions, LoginForm, RegisterForm } from "../../components/Auth";
 import "./Auth.scss";
 
 export function Auth() {
-  return (
-    <div>
-      <Button primary>Auth Screen</Button>
-    </div>
-  );
+  const [typeForm, setTypeForm] = useState(null);
+
+  const openLogin = () => setTypeForm("login");
+  const openRegister = () => setTypeForm("register");
+  const goBack = () => setTypeForm(null);
+
+  const renderForm = () => {
+    if (typeForm === "login") {
+      return <LoginForm openRegister={openRegister} goBack={goBack} />;
+    } else if (typeForm === "register") {
+      return <RegisterForm openLogin={openLogin} goBack={goBack} />;
+    }
+    return <AuthOptions openLogin={openLogin} openRegister={openRegister} />;
+  };
+
+  return <div>{renderForm()}</div>;
 }
